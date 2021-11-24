827a585428788ee9cfe1071f87be3bc0657c4452
Demyst1fy <demyst1fy@outlook.com>
2021-11-24 21:38:47 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./App.css";
 import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
-function App() {
+const App = () => {
   return (
     <div className="container">
       <Header />
       <Map />
-      <Button color="green" text="Show Heat Islands"></Button>
-      <Button color="blue" text="Show Public green spaces"></Button>
-      <Button color="green" text="Show water areas"></Button>
+      <Button color="green" text="Show Heat Islands" />
+      <Button color="blue" text="Show Public green spaces" />
+      <Button color="green" text="Show water areas" />
     </div>
   );
-}
+};
 
 export default App;
