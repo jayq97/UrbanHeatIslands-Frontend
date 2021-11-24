f9edc643491e1737d4ccb5919dcbb76d62d3a2ba
karliHase <if20b158@technikum-wien.at>
2021-11-24 22:18:22 +0100

    init commit

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,18 @@
 import "./Map.css";
+import React, {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
+//import * as weatherStation from ""; Wetterstation
 
 const Map = () => {
-  return <div className="map"></div>;
+  return (
+    <MapContainer center={[48.210033, 16.363449]} zoom={14}scrollWheelZoom={true}>
+      <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
 };
 
 export default Map;
