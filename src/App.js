f26b16eb9e47d9beef7db2e091de7854a2392124
Demyst1fy <demyst1fy@outlook.com>
2022-03-21 21:40:46 +0100

    Change Title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import Map from "./components/Map/Map";
-import SelectDistrict from "./components/SelectDistrict/SelectDistrict";
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  const [district, setDistrict] = useState("1");
-
   return (
     <>
       <Navbar />
