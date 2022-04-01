8d95bedaf3dad24fc7c4da24cc5bd57ec633ae29
Demyst1fy <demyst1fy@outlook.com>
2022-04-01 13:35:33 +0200

    Updated Imprint

--- src/screens/imprint.js
+++ src/screens/imprint.js
@@ -4,33 +4,69 @@ const Imprint = () => {
   return (
     <div
       style={{
-        textAlign: "center",
+        textAlign: "left",
+        maxWidth: "75%",
       }}
+      className="container"
     >
       <h1>Impressum</h1>
-      <h3>Angaben gemäß § 5 TMG</h3>
       <p>
-        <br />
-        FH-Technikum Wien
-        <br />
-        Höchstädtplatz 6<br />
-        1200 Wien
-        <br />
-        Österreich
+        Offenlegungspflicht laut §25 Mediengesetz und Informationspflicht laut
+        §5 E-Commerce Gesetz
       </p>
-      <h3>Kontakt & Redaktioneller Verantwortlicher</h3>
+      <br />
+      <h3>Medieninhaber und Herausgeber</h3>
+      <p>Verein Fachhochschule Technikum Wien</p>
+      <br />
+      <h3>Geschäftsführung</h3>
       <p>
+        Mariahilfer Straße 37-39 1060 Wien
         <br />
-        FH-Technikum Wien:{" "}
+        T: +43 1 588 39-0
+        <br />
+        F: +43 1 588 39-49
+        <br />
+        E:{" "}
         <a href="mailto:if20b144@technikum-wien.at">info@technikum-wien.at</a>
         <br />
       </p>
       <br />
-      <h3>Verbraucherstreitbeilegung/ Universalschlichtungsstelle</h3>
+      <h3>Urheberrecht</h3>
       <p>
-        <br />
-        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
-        vor einer Verbraucherschlichtungsstelle teilzunehmen.
+        Das Layout der Website, die verwendeten Grafiken und Bilder, sowie die
+        Inhalte der einzelnen Seiten und Beiträge sind urheberrechtlich
+        geschützt. Alle Rechte, auch die der fotomechanischen Wiedergabe, der
+        Vervielfältigung und der Verbreitung mittels besonderer Verfahren – zum
+        Beispiel Datenverarbeitung, Datenträger und Datennetze – auch teilweise,
+        behält sich das Team der FH Technikum Wien vor.
+      </p>
+      <br />
+      <h3>Bilder</h3>
+      <p>
+        Alle auf der Website res.technikum-wien.at verwendeten Bilder, welche
+        nicht ausdrücklich mit der jeweiligen Quelle versehen sind, wurden
+        entweder für die Gestaltung der Webseite zur freien Verwendung zur
+        Verfügung gestellt oder sind ohne Urheberrechtsschutz (CC0) verfügbar.
+      </p>
+      <br />
+      <h3>Haftungsausschluss</h3>
+      <p>
+        Das Team der FH Technikum Wien ist bestrebt, dass alle auf
+        res.technikum-wien.at präsentierten Informationen inhaltlich korrekt und
+        stets auf dem neuesten Stand sind. Die FH Technikum Wien übernimmt
+        jedoch keine Gewähr für die Richtigkeit, Vollständigkeit und Aktualität
+        der präsentierten Inhalte. Eine Haftung der FH Technikum Wien ist daher
+        ausgeschlossen. <br /> <br />
+        Seiten und Beiträge welche auf dieser Webseite zu finden sind, können
+        eingebettete Inhalte beinhalten (z. B. Bilder, Videos, etc.). Alle Links
+        zu externen Webseiten sowie zu eingebetteten externen Webseiten werden
+        sorgfältig ausgewählt. Das Team der FH Technikum Wien ist stets bemüht,
+        Inhalte externer Webseiten zu prüfen. Diese externen Webseiten können
+        Daten über die BesucherInnen sammeln, Cookies benutzen, zusätzliche
+        Tracking-Dienste einbetten etc. Wir weisen ausdrücklich darauf hin, dass
+        wir keinen Einfluss auf die Inhalte externer Webseiten haben! Wenn Ihnen
+        unangemessene Inhalte auffallen, informieren Sie uns bitte! Die
+        Kontaktdaten finden Sie im Impressum.
       </p>
     </div>
   );
